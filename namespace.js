fa2cb1cad8220e373451a0057a123d1af797315d
adamocef <cefa0007@algonquinlive.com>
2017-10-17 17:03:32 -0400

    new commit

--- namespace.js
+++ namespace.js
@@ -1,69 +1,26 @@
-var
-CEFA0007 = {
-    init: function(){
-        console.log("script in PROF3SSORST3V3 called");
+var Cefa0007 = {
+    init: function () {
+        let adamoDiv = document.createElement("div");
+        adamoDiv.classList.add("box");
+        adamoDiv.textContent = "cefa0007";
+        let myBox = document.querySelector("#boxes");
+        adamoDiv.addEventListener("click",Cefa0007.boxClicked);
+        adamoDiv.addEventListener("mouseover",Cefa0007.boxMouseOver);
+        adamoDiv.addEventListener("mouseout",Cefa0007.boxMouseOut);
+        
+        myBox.appendChild(adamoDiv);
+    },
+    boxClicked : function (ev) {
+        ev.currentTarget.style.borderColor = "blue";
+        ev.currentTarget.style.backgroundColor = "green";
+    },
+    boxMouseOver: function (ev) {
+        ev.currentTarget.classList.toggle("highlight");
+    },
+    boxMouseOut: function (ev) {
+        ev.currentTarget.classList.toggle("highlight");
     }
-};
-
-var b = document.querySelector('body');
-var m = document.querySelector('main');
-var d = document.querySelector('div');
-var p = document.querySelector('p');
-
-//can put false at the end of gold to make colours come towards to  where you clicked
-
-// 
-
-m.addEventListener("click",toggleGoldm);
-m.addEventListener("mouseover",toggleGoldm);
-d.addEventListener("mouseout",toggleGoldd);
-
-d.addEventListener("click",toggleGoldd);
-d.addEventListener("mouseover",toggleGoldd);
-d.addEventListener("mouseout",toggleGoldd);
-
-
-
-function toggleGoldp(ev){
-  var classes = ['highlight','a','b','c'];
-  var num =  Math.floor(Math.random()*4) ;
-  var cls = classes [num];
-  ev.currentTarget.classList.toggle(cls);
 }
-
-
-function toggleGoldm(ev){
-  // ev.stopPropagation();
-  // ev.stopImmediatePropagation();
-  var classes = ['highlight','a','b','c'];
-  var num =  Math.floor(Math.random()*4) ;
-  var cls = classes [num];
-  ev.currentTarget.classList.toggle(cls);
-}
-
-
-
-function toggleGoldd(ev){
-  var classes = ['highlight','a','b','c'];
-  var num =  Math.floor(Math.random()*4) ;
-  var cls = classes [num];
-  ev.currentTarget.classList.toggle(cls);
-}
-
-
-
-function toggleGoldb(ev){
-  var classes = ['highlight','a','b','c'];
-  var num =  Math.floor(Math.random()*4) ;
-  let cls = classes [num];
-  ev.currentTarget.classList.toggle(cls);
-}
-
-var a =document.querySelector('a');
-a.addEventListener("mouseover",function(ev){
-
-});
-
 
 
 
